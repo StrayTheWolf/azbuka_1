c06c537f49bdf6e2f880ce1e775bca7e89df24b0
Wolf Stanislav <ork2004@yandex.ru>
2021-02-14 22:36:17 +0300

    working on header

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,9 @@
 let mix = require('laravel-mix');
 
+mix.browserSync('http://localhost:63342/azbuka/dist/index.html?_ijt=o0k61q8k7jjq0f2ds85lsgkfqs')
+
 mix.sass('src/scss/main.scss', 'dist/css');
 mix.js('src/script.js', 'dist/script-dist.js');
 
-mix.copy('src/index.html', 'dist/index.html');
+mix.copy('src/index.html', 'dist/index.html').options({processCssUrls : false});
 mix.copyDirectory('src/images', 'dist/images');
